730e8d92380296ae70aad9fdf5dcfb36a1613f8c
Bob McNabb <robertmcnabb@gmail.com>
2026-04-02 15:04:53 +0100

    add scripts for IL aerial images

---
+++ iceland/aerial_images/1980_08_22/preprocessed_images/bundle_adjust.py
@@ -0,0 +1,52 @@
+import os
+from pathlib import Path
+import pandas as pd
+import geopandas as gpd
+import numpy as np
+from spymicmac import micmac, register
+
+
+os.makedirs('plots', exist_ok=True)
+
+globstr = 'F-*.tif'
+crs = 32627
+
+models = ['RadialBasic', 'RadialExtended', 'FraserBasic', 'FraserExtended']
+
+gcps = pd.read_csv('GCPs.txt', delimiter=' ', header=2, names=['id', 'x', 'y', 'elevation'])
+gcps['id'] = gcps['id'].astype(str)
+
+gcps = gpd.GeoDataFrame(gcps, geometry=gpd.points_from_xy(gcps.x, gcps.y, crs=crs))
+gcps.drop(columns=['x', 'y']).to_file('GCPs.gpkg')
+
+for cam in models:
+    gcps = gpd.read_file('GCPs.gpkg')
+    gcps = micmac.bascule(
+        gcps,
+        outdir='.',
+        img_pattern=globstr.replace('*.', '.*'),
+        sub='',
+        ori=cam,
+        outori='Init' + cam,
+        fn_gcp='GCPs',
+        fn_meas='Measures'
+    )
+
+    gcps = micmac.campari(
+        gcps,
+        outdir='.',
+        img_pattern=globstr.replace('*.', '.*'),
+        sub='',
+        fn_gcp='GCPs',
+        fn_meas='Measures',
+        inori='Init' + cam,
+        outori='Terrain' + cam,
+        allfree=True,
+        sig_abs=1,
+        sig_pix=0.5
+    )
+
+    gcps['camp_xy'] = np.sqrt(gcps.camp_xres ** 2 + gcps.camp_yres ** 2)
+
+    fig = register._plot_residuals(gcps.dropna())
+    fig.savefig(Path('plots', f"{cam}_gcp_residuals.png"), bbox_inches='tight', dpi=200)
---
+++ iceland/aerial_images/1980_08_22/preprocessed_images/register_preproc.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+from pathlib import Path
+from glob import glob
+from spymicmac import micmac, register
+
+
+globstr = 'F-*.tif' # fill in image pattern
+local_crs = 32627 # need to fill in the crs to use
+prefix = 'IL' # fill in prefix for study site
+do_ortho = True # whether to make the ortho images
+ori = 'FraserExtended' # change orientation directory if needed
+
+glacmask = None # fill in path to optional glacier mask
+landmask = Path('..', '..', '..', 'aux_dems', f"{prefix}_reference_dem_large_mask.tif") # fill in path to optional land mask
+fn_ref = Path('..', '..', '..', 'aux_dems', f"{prefix}_reference_dem_large.tif") # fill in path to optional land mask
+
+# create the relative dem/orthophoto
+if not Path(f"MEC-Rel{ori}").exists():
+
+    micmac.malt(
+        globstr.replace('*.', '.*'),
+        ori,
+        dirmec=f"MEC-Rel{ori}",
+        zoomf=2,
+        cost_trans=4,
+        szw=3,
+        regul=0.1,
+        do_ortho=do_ortho
+    )
+
+    if do_ortho:
+        imlist = sorted(glob(globstr))
+        for fn in imlist:
+            micmac.mosaic_micmac_tiles(
+                filename=fn,
+                dirname=f"Ortho-MEC-Rel{ori}",
+            )
+
+    micmac.mosaic_micmac_tiles(
+        filename='Z_Num8_DeZoom2_STD-MALT',
+        dirname=f"MEC-Rel{ori}"
+    )
+
+# register the images to the reference DEM
+register.register_relative(
+    f"MEC-Rel{ori}",
+    fn_ref,
+    glacmask=glacmask,
+    landmask=landmask,
+    ori=ori,
+    density=200,
+    strategy='peaks',
+    use_hillshade=True,
+    subscript=ori
+)
