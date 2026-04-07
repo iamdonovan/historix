35290e298d633fb486cd3b000dc39a849acf970a
Bob McNabb <robertmcnabb@gmail.com>
2026-04-07 09:09:03 +0100

    use asp.gcps_from_dem
    - move subprocess calls, etc. to spymicmac.asp
    - use cropped dem for heights in ba
    - use lon-lat-height gcp error, not ecef

--- casa_grande/kh9_pc_images/1978_03_25/preprocessed_images/dem_bundle_adjust.py
+++ casa_grande/kh9_pc_images/1978_03_25/preprocessed_images/dem_bundle_adjust.py
@@ -1,65 +1,47 @@
+import os
 import subprocess
 from pathlib import Path
-from spymicmac import asp
+from spymicmac import asp, data
 
 
-do_corr = True
-
-p = subprocess.Popen(['gdaldem', 'hillshade', '-multidirectional', '-compute_edges',
-                      'dem_blur.tif', 'dem_blur_hs.tif'])
-p.wait()
+map_res = 12 # resolution to project images to
 
 # read the pairs file
 with open('overlaps.txt', 'r') as f:
     pairs = [l.strip() for l in f.readlines()]
 
 for nn, pair in enumerate(pairs, start=1):
-    left, right = pair.split()
-
-    p = subprocess.Popen(['dem_mosaic', '--dem-blur-sigma', '5', f"st_small/pair{nn}-DEM.tif",
-                          '-o', f"dem_lowres_pair{nn}.tif"])
-    p.wait()
-
-    p = subprocess.Popen(['gdaldem', 'hillshade', '-multidirectional', '-compute_edges',
-                          f"dem_lowres_pair{nn}.tif", f"dem_lowres_pair{nn}_hs.tif"])
-    p.wait()
-
-    if do_corr:
-        p = subprocess.Popen(['parallel_stereo', '--correlator-mode', '--stereo-algorithm', 'asp_mgm',
-                              '--subpixel-mode', '9', '--ip-per-tile', '1000',
-                              f"dem_lowres_pair{nn}_hs.tif", 'dem_blur_hs.tif', f"warp/run{nn}"])
-        p.wait()
-
-
-    p = subprocess.Popen(['dem2gcp', '--warped-dem', f"dem_lowres_pair{nn}.tif", '--ref-dem', 'dem_blur.tif',
-                          '--warped-to-ref-disparity', f"warp/run{nn}-F.tif",
-                          '--left-image', left,
-                          '--right-image', right,
-                          '--left-camera', f"ba/all-{left.replace('.tif', '')}.tsai",
-                          '--right-camera', f"ba/all-{right.replace('.tif', '')}.tsai",
-                          '--match-file', f"ba/all-{left.replace('.tif', '')}__{right.replace('.tif','')}-clean.match",
-                          '--gcp-sigma', '1.0',
-                          '--max-num-gcp', '20000',
-                          '--output-gcp', f"pair{nn}_dem.gcp"
-                          ])
-    p.wait()
+    if not Path('st_small', f"pair{nn}-DEM.tif").exists():
+        print(f"No DEM found for pair {nn}, skipping.")
+        continue
+    else:
+        left, right = pair.split()
+        asp.gcps_from_dem(
+            (left, right),
+            fn_dem = f"st_small/pair{nn}-DEM.tif",
+            fn_ref = 'dem_cropped.tif',
+            camera_prefix = 'ba/all',
+            fn_gcp = f"pair{nn}_dem.gcp",
+            warp_prefix = f"warp/run{nn}"
+        )
 
 
 ba_kwargs = {
     'intrinsics-to-float': 'all',
     'intrinsics-to-share': 'focal_length',
-    'heights-from-dem': 'dem_blur.tif',
+    'heights-from-dem': 'dem_cropped.tif',
     'heights-from-dem-uncertainty': 10000,
     'ip-per-image': 100000,
     'ip-inlier-factor': 1000,
-    'remove-outliers-params': "75.0 3.0 10 20",
+    'remove-outliers-params': "75.0 3.0 3 5",
     'overlap-list': 'overlaps.txt',
-    'match-files-prefix': 'ba/all',
+    #'match-files-prefix': 'ba/all',
 }
 
 ba_flags = [
     'inline-adjustments',
-    'solve-intrinsics'
+    'solve-intrinsics',
+    'use-lon-lat-height-gcp-error'
 ]
 
 asp.bundle_adjust(
@@ -79,6 +61,6 @@ for fn_img in imlist:
         fn_dem = 'dem_blur.tif',
         fn_img = fn_img + '.tif',
         fn_cam = Path('ba', f"all-{fn_img}.tsai"),
-        res = 10,
+        res = map_res,
         fn_out = fn_img + '.map-ba.tif'
     )
