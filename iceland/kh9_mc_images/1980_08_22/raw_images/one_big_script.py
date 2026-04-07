411856e2e2863181f1a0a9c35226c0562d0a287d
Bob McNabb <robertmcnabb@gmail.com>
2026-04-07 13:48:27 +0100

    update one_big_script for multiple experiments
    use both ortho and dem (large) to find gcps

--- iceland/kh9_mc_images/1980_08_22/raw_images/one_big_script.py
+++ iceland/kh9_mc_images/1980_08_22/raw_images/one_big_script.py
@@ -10,7 +10,8 @@ prefix = 'IL'
 
 glacmask = None # fill in path to optional glacier mask
 landmask = Path('..', '..', '..', 'aux_dems', f"{prefix}_reference_dem_large_mask.tif") # fill in path to optional land mask
-fn_ref = Path('..', '..', '..', 'aux_dems', f"{prefix}_reference_dem_large.tif") # fill in path to optional land mask
+fn_dem = Path('..', '..', '..', 'aux_dems', f"{prefix}_reference_dem_large.tif") # fill in path to reference dem
+fn_ref = Path('..', '..', '..', 'aux_orthos', f"{prefix}_orthomosaic_large.tif") # fill in path to reference ortho
 do_ortho = False
 
 # preprocess images
@@ -43,6 +44,13 @@ for fn in imlist:
         dirname='Ortho-MEC-Relative',
     )
 
+micmac.tawny('Ortho-MEC-Relative')
+
+micmac.mosaic_micmac_tiles(
+    filename='Orthophotomosaic',
+    dirname='Ortho-MEC-Relative',
+)
+
 micmac.mosaic_micmac_tiles(
     filename='Z_Num8_DeZoom2_STD-MALT',
     dirname='MEC-Relative',
@@ -51,16 +59,28 @@ micmac.mosaic_micmac_tiles(
 # register the images to the reference DEM
 register.register_relative(
     'MEC-Relative',
-    fn_ref,
+    fn_dem,
     glacmask=glacmask,
     landmask=landmask,
     density=400,
     strategy='peaks',
-    use_hillshade=True
+    use_hillshade=True,
+    subscript='dem'
+)
+
+register.register_relative(
+    'MEC-Relative',
+    fn_dem,
+    fn_ref=fn_ref,
+    useortho=True,
+    glacmask=glacmask,
+    landmask=landmask,
+    density=400,
+    strategy='peaks',
+    subscript='ortho'
 )
 
 malt_args = {
-    'dirmec': 'MEC-Malt',
     'zoomf': 1,
     'cost_trans': 4,
     'szw': 3,
@@ -71,25 +91,29 @@ malt_args = {
 # create the absolute dem/orthophotos
 # TODO: check if this is too big and iterate if needed
 imlist = sorted(glob('OIS*.tif'))
-micmac.block_malt(
-    imlist,
-    ori='TerrainFinal',
-    nimg=2,
-    malt_args=malt_args
-)
 
-# clean up the outputs
-for ind, block in enumerate(glob(f"MEC-Malt_block*/")):
-    # create the orthomosaic
-    if do_ortho:
-        micmac.tawny(block)
+for ref in ['dem', 'ortho']:
+    malt_args['dirmec'] = f"MEC-Malt_{ref}"
+
+    micmac.block_malt(
+        imlist,
+        ori=f"TerrainFinal_{ref}",
+        nimg=2,
+        malt_kwargs=malt_args
+    )
 
     # clean up the outputs
-    micmac.post_process(
-        projstr=local_crs,
-        out_name=f"{out_name}_block{ind}",
-        dirmec=block,
-        do_ortho=do_ortho,
-        ind_ortho=False,
-        do_ply=True
-    )
+    for ind, block in enumerate(glob(f"MEC-Malt_{ref}_block*/")):
+        # create the orthomosaic
+        if do_ortho:
+            micmac.tawny(block)
+
+        # clean up the outputs
+        micmac.post_process(
+            projstr=local_crs,
+            out_name=f"{out_name}_{ref}_block{ind}",
+            dirmec=block,
+            do_ortho=do_ortho,
+            ind_ortho=False,
+            do_ply=True
+        )
