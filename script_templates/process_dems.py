8bfe5166a6e89349406bb5229b7932b5ce3b17ed
Bob McNabb <robertmcnabb@gmail.com>
2026-04-08 17:25:24 +0100

    remove individual process_dems scripts
    - keep central script that can be called to run on /tmp
    - pass parameters as command-line arguments

--- script_templates/process_dems.py
+++ script_templates/process_dems.py
@@ -1,46 +1,87 @@
 #!/usr/bin/env python
+import argparse
+from pathlib import Path
 from glob import glob
 from spymicmac import micmac
 
 
-globstr = '' # fill in image pattern
-local_crs = '' # need to fill in the crs to use
-prefix = '' # fill in prefix for study site
-do_ortho = False # whether to make the ortho images
+def main():
+    parser = argparse.ArgumentParser(description="process DEMs for an orientation directory",
+                                     formatter_class=argparse.RawDescriptionHelpFormatter)
+    parser.add_argument('ori', action='store', type=str,
+                        help='Name of orientation directory (after Ori-).')
+    parser.add_argument('globstr', action='store', type=str,
+                        help='Matching pattern for images to process (e.g., DZB*.tif).')
+    parser.add_argument('local_crs', action='store', type=str,
+                        help='EPSG code for the output CRS (e.g., 32627).')
+    parser.add_argument('prefix', action='store', type=str,
+                        help='Prefix for the HISTORIX test cite (should be either CG or IL).')
+    parser.add_argument('out_dir', action='store', type=str,
+                        help='Output directory for results.')
+    parser.add_argument('--do_ortho', action='store_true', type=bool,
+                        help='Process ortho images as part of Malt (default: False).')
+    parser.add_argument('--as_block', action='store_true', type=bool,
+                        help='Process using spymicmac.block_malt (default: False).')
 
-# update orientation names as needed
-for ori in ['RadialBasic', 'RadialExtended', 'FraserBasic', 'FraserExtended', 'Final_FraserExtended']:
+    args = parser.parse_args()
+    print(args)
 
     malt_kwargs = {
-        'dirmec': f"MEC-{ori}",
+        'dirmec': f"MEC-{args.ori}",
         'zoomf': 1,
         'cost_trans': 4,
         'szw': 3,
         'regul': 0.1,
-        'do_ortho': do_ortho,
+        'do_ortho': args.do_ortho,
         'clean': True
     }
 
     # create the absolute dem/orthophotos
-    imlist = sorted(glob(globstr))
-    micmac.block_malt(
-        imlist,
-        ori,
-        nimg=2,
-        malt_kwargs=malt_kwargs
+    if args.block_malt:
+        imlist = sorted(glob(args.globstr))
+        micmac.block_malt(
+            imlist,
+            f"Terrain{args.ori}",
+            nimg=2,
+            malt_kwargs=malt_kwargs
+        )
+        for ind, block in enumerate(glob(f"MEC-{args.ori}_block*/")):
+            # create the orthomosaic
+            if args.do_ortho:
+                micmac.tawny(block)
+
+            # clean up the outputs
+            micmac.post_process(
+                out_dir=Path(args.out_dir, 'post_processed'),
+                projstr=args.local_crs,
+                out_name=f"Terrain{args.ori}_block{ind}",
+                dirmec=block,
+                do_ortho=args.do_ortho,
+                ind_ortho=False,
+                do_ply=True
+            )
+
+    else:
+        micmac.malt(
+            args.globstr.replace('*.', '.*'),
+            f"Terrain{args.ori}",
+            **malt_kwargs
+        )
+
+    if args.do_ortho:
+        micmac.tawny(f"MEC-{args.ori}")
+
+    # clean up the outputs
+    micmac.post_process(
+        out_dir=Path(args.out_dir, 'post_processed'),
+        projstr=args.local_crs,
+        out_name=f"Terrain{args.ori}",
+        dirmec=f"MEC-{args.ori}",
+        do_ortho=args.do_ortho,
+        ind_ortho=False,
+        do_ply=True
     )
 
-    for ind, block in enumerate(glob(f"MEC-{ori}_block*/")):
-        # create the orthomosaic
-        if do_ortho:
-            micmac.tawny(block)
 
-        # clean up the outputs
-        micmac.post_process(
-            projstr=local_crs,
-            out_name=f"Terrain{ori}_block{ind}",
-            dirmec=block,
-            do_ortho=do_ortho,
-            ind_ortho=False,
-            do_ply=True
-        )
+if __name__ == "__main__":
+    main()
