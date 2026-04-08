83777a26a53a85a2e41e832a3af2709100fe3b9e
Bob McNabb <robertmcnabb@gmail.com>
2026-04-08 17:24:00 +0100

    add script for processing dems on /tmp

---
+++ script_templates/process_malt_tmp.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+tmpdir=/tmp/scratch/malt
+cwd=$(pwd)
+
+globstr=''
+crs=''
+prefix=''
+
+mkdir -p $tmpdir
+cp -v $globstr $tmpdir
+
+cat ori_list.txt | while read ori; do
+  cp -rv Ori-$ori $tmpdir;
+done
+
+cp ori_list.txt $tmpdir
+
+cd $tmpdir
+
+cat ori_list.txt | while read ori; do
+  python ~/data/historix/scripts/process_dems.py $ori "$globstr" $crs $prefix $cwd #--as_block
+
+  rm -r MEC-$ori*
+done
